8669c0f514ff93b9f58b75b57d005cc1b90d08a8
codescaptain <ahmet-57-@hotmail.com>
2023-07-02 22:06:44 +0300

    [airbnb-clone-04] create login/logout modal

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,14 +1,20 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 import {enter, leave, toggle} from 'el-transition'
+
 export default class extends Controller {
 
-  static targets = [ "openUserMenu" ]
+    static targets = ["openUserMenu", "userAuthLink"]
 
-  connect() {
-    this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
-  }
+    connect() {
+        this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+        this.userAuthLinkTargets.forEach(link => {
+            link.addEventListener('click', () => {
+                enter(document.getElementById('modal-wrapper'))
+            })
+        })
+    }
 
-  toggleDropdownMenu(){
-    toggle(document.getElementById("menu-dropdown-items"))
-  }
+    toggleDropdownMenu() {
+        toggle(document.getElementById("menu-dropdown-items"))
+    }
 }
